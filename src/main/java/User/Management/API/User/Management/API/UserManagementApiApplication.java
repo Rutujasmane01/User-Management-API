aab49c301a82b07e3a119591d40bae5d3778c32f
Rutuja Mane <rutujasmane01@gmail.com>
2026-03-28 15:02:03 +0530

    added requied dependencies

---
+++ src/main/java/User/Management/API/User/Management/API/UserManagementApiApplication.java
@@ -0,0 +1,13 @@
+package User.Management.API.User.Management.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/User/Management/API/User/Management/API/UserManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package User.Management.API.User.Management.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
